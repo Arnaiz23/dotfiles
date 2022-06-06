62d81906e4bc185b247d21abe4834deb06453c76
Adrián Arnaiz Fernández <82878210+Arnaiz23@users.noreply.github.com>
2022-06-06 11:20:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Plugins zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Change zsh
 chsh -s `which zsh`
