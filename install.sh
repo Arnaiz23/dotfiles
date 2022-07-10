18df224da6e0674c2d8d861a0ec7b2ce8f309b8a
Arnaiz23 <aaf110501@gmail.com>
2022-07-10 23:51:14 +0200

    Modify the script

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo pacman -Syu
 sudo pacman -S \
 	sudo vim nano git bat zsh wget curl lsb-release \
 	jq less catimg tldr man neofetch htop unzip zip \
-	exa lolcat gpg locate zgen net-tools nmap ncal neovim
+	exa lolcat gpg locate net-tools nmap ncal neovim
 
 # Fix batcat -> cat (Debian)
 sudo ln -s /usr/bin/batcat /usr/local/bin/bat
@@ -22,6 +22,9 @@ sudo ln -s /usr/bin/batcat /usr/local/bin/bat
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo source $HOME/.dotfiles/.zshrc >> ~/.zshrc
+
+# Install zgen
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 
 # Node
@@ -41,11 +44,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # Modify vim/nvim
 
-git clone https://github.com/Arnaiz23/Vim.git >> .vimconfigure
+git clone https://github.com/Arnaiz23/Vim.git  .vimconfigure
 cp .vimconfigure/.vimrc ~/.vimrc
 
 mkdir ~/.config/nvim
-echo "source ~/.vimrc" > ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
