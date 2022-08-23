d582884799c794e19feb739b0a51485be1ead7ec
Arnaiz23 <aaf110501@gmail.com>
2022-08-23 12:43:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,8 +48,11 @@ nvm install --lts
 cp ~/.dotfiles/.vimrc ~/
 
 # Create and copy the configuration for nvim
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 cp ~/.dotfiles/init.vim ~/.config/nvim/
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
