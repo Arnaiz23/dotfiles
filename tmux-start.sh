69f6a3f5b33c5ae13f631165fac0fe4e64d85021
Arnaiz23 <aaf110501@gmail.com>
2022-10-02 13:26:23 +0200

    Create tmux script editor

--- tmux-start.sh
+++ tmux-start.sh
@@ -3,23 +3,21 @@
 session="ide"
 
 tmux has-session -t $session
-if [ "$?" -eq 1 ] ; then
-
-	# Create a new session named "$session", and run command
-	tmux new-session -d -s "$session"
-
-	window=0
-	tmux rename-window -t $session:$window 'editor'
-	tmux send-keys -t $session:$window 'v' Enter
-
-	window=1
-	tmux new-window -t $session:$window -n 'zsh'
-	tmux split-window -t $session:$window -h
-	tmux select-pane -t $session:$window -t 0
-	tmux split-window -t $session:$window -v
-
-	tmux select-window -t $session:0
-
+if [ "$?" -eq 0 ] ; then
+	tmux kill-ses -t$session
 fi
+
+# Create a new session named "$session", and run command
+tmux new-session -d -s "$session"
+
+window=0
+tmux rename-window -t $session:$window 'editor'
+tmux send-keys -t $session:$window 'v' Enter
+
+window=1
+tmux new-window -t $session:$window -n 'zsh'
+tmux split-window -t $session:$window -h
+tmux select-pane -t $session:$window -t 0
+tmux split-window -t $session:$window -v
 
 tmux a -tide
