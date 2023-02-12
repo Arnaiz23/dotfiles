ef6b119ff572fcc549b04c1897837639deaff3b5
Arnaiz23 <aaf110501@gmail.com>
2023-02-12 14:24:23 +0100

    feat: 🎸 If exists ide, attach

--- scripts/tmux-start.sh
+++ scripts/tmux-start.sh
@@ -4,20 +4,22 @@ session="ide"
 
 tmux has-session -t $session
 if [ "$?" -eq 0 ] ; then
-	tmux kill-ses -t$session
+	#tmux kill-ses -t$session
+	tmux a -tide
+else
+	# Create a new session named "$session", and run command
+	tmux new-session -d -s "$session"
+
+	window=0
+	tmux rename-window -t $session:$window 'editor'
+	tmux send-keys -t $session:$window 'v' Enter
+
+	window=1
+	tmux new-window -t $session:$window -n 'zsh'
+	tmux split-window -t $session:$window -h -p 90
+	tmux select-pane -t $session:$window -t 0
+	tmux split-window -t $session:$window -v
+
+	tmux a -tide
 fi
 
-# Create a new session named "$session", and run command
-tmux new-session -d -s "$session"
-
-window=0
-tmux rename-window -t $session:$window 'editor'
-tmux send-keys -t $session:$window 'v' Enter
-
-window=1
-tmux new-window -t $session:$window -n 'zsh'
-tmux split-window -t $session:$window -h -p 90
-tmux select-pane -t $session:$window -t 0
-tmux split-window -t $session:$window -v
-
-tmux a -tide
