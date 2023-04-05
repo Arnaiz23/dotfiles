ca36d93314415b76c1507dece6bd8f1fcc15fd22
Arnaiz23 <aaf110501@gmail.com>
2023-04-05 17:06:29 +0200

    fix: Add plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,4 +9,10 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   -- Your plugins go here
+  -- Theme
+  use 'joshdick/onedark.vim'
+  -- File Explorer
+  use 'nvim-tree/nvim-tree.lua'
+  -- Autocomplete parenthesis and others
+  use 'jiangmiao/auto-pairs'
 end)
