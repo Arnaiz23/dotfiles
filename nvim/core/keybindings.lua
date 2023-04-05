98e41d1437810b9416f7a2c219de59a2907dae9b
Arnaiz23 <aaf110501@gmail.com>
2023-04-05 18:00:03 +0200

    fix: Modify the function

--- nvim/core/keybindings.lua
+++ nvim/core/keybindings.lua
@@ -4,43 +4,43 @@ vim.g.mapleader = ","
 
 local opts = { noremap = true, silent = true }
 
-local function map(mode, shorcut, command)
+function Map(mode, shorcut, command)
 	vim.api.nvim_set_keymap(mode, shorcut, command, opts)
 end
 
-local function nmap(shorcut, command)
-	map('n', shorcut, command)
+function Nmap(shorcut, command)
+	Map('n', shorcut, command)
 end
 
 -- move beetween panels
-nmap('<C-h>', '<C-w>h')
-nmap('<C-l>', '<C-w>l')
-nmap('<C-j>', '<C-w>j')
-nmap('<C-k>', '<C-w>k')
+Nmap('<C-h>', '<C-w>h')
+Nmap('<C-l>', '<C-w>l')
+Nmap('<C-j>', '<C-w>j')
+Nmap('<C-k>', '<C-w>k')
 
 -- Work with the nvim tree
-nmap('<leader>e', ':NvimTreeToggle<CR>')
-nmap('<leader>o', ':NvimTreeFocus<CR>')
+Nmap('<leader>e', ':NvimTreeToggle<CR>')
+Nmap('<leader>o', ':NvimTreeFocus<CR>')
 
 -- GIT
-nmap('<leader>gb', ':Telescope git_branches<CR>')
-nmap('<leader>gc', ':Telescope git_commits<CR>')
+Nmap('<leader>gb', ':Telescope git_branches<CR>')
+Nmap('<leader>gc', ':Telescope git_commits<CR>')
 --map('<leader>gl', ':Lazygit<CR>', opts)
 
 -- Save
-nmap('<C-s>', ':w<CR>')
+Nmap('<C-s>', ':w<CR>')
 
 -- Quit
-nmap('<leader>q', ':q<CR>')
+Nmap('<leader>q', ':q<CR>')
 
 -- Find Files
-nmap('<leader>f', ':Telescope find_files hidden=true<CR>')
+Nmap('<leader>f', ':Telescope find_files hidden=true<CR>')
 
 -- Change words
-nmap('<leader>g', ':% s/')
+Nmap('<leader>g', ':% s/')
 
 -- Open new tab
-nmap('<leader>t', ':tabnew<CR>')
+Nmap('<leader>t', ':tabnew<CR>')
 
 -- Show all the maps
-nmap('<leader>m', ':Maps<CR>')
+Nmap('<leader>m', ':Maps<CR>')
