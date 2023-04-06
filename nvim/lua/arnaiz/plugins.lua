48ce0c4510197c8de759f32696dc0840f343ec4f
Arnaiz23 <aaf110501@gmail.com>
2023-04-06 13:03:34 +0200

    feat: 🎸 Add more plugins

--- nvim/lua/arnaiz/plugins.lua
+++ nvim/lua/arnaiz/plugins.lua
@@ -9,10 +9,21 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   -- Your plugins go here
-  use 'joshdick/onedark.vim'            -- Theme
-  use 'nvim-tree/nvim-tree.lua'         -- File Explorer
-  use 'jiangmiao/auto-pairs'            -- Autocomplete parenthesis and others
-  use 'kyazdani42/nvim-web-devicons'    -- Icons
-  use 'nvim-lualine/lualine.nvim'       -- Lualine
-  use 'nvim-treesitter/nvim-treesitter' -- Colorized sintax
+  use 'joshdick/onedark.vim'                 -- Theme
+  use 'nvim-tree/nvim-tree.lua'              -- File Explorer
+  use 'jiangmiao/auto-pairs'                 -- Autocomplete parenthesis and others
+  use 'kyazdani42/nvim-web-devicons'         -- Icons
+  use 'nvim-lualine/lualine.nvim'            -- Lualine
+  use 'nvim-treesitter/nvim-treesitter'      -- Colorized sintax
+  use 'dense-analysis/ale'                   -- Prettier when save the file
+  use 'alvan/vim-closetag'                   -- Close the tags automatic
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'        -- Telescope
+  -- use 'nvim-telescope/telescope-file-browser.nvim' -- File browser <F3>
+  use 'lewis6991/gitsigns.nvim'              -- Gitsigns
+  use 'norcalli/nvim-colorizer.lua'          -- Background color to the hex code #fff
+  use 'princejoogie/tailwind-highlight.nvim' -- Background color tailwind
+  use 'neovim/nvim-lspconfig'                -- Configurations for Nvim LSP
+  use 'glepnir/lspsaga.nvim'                 -- LSP UI
+  use 'lukas-reineke/indent-blankline.nvim'  -- Indent plugin configuration
 end)
