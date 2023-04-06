3c3eb65722adda2f6c65e1927e0a7395d291cb75
Arnaiz23 <aaf110501@gmail.com>
2023-04-06 20:52:21 +0200

    fix: modify the telescope config

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,13 +1,4 @@
 local status, telescope = pcall(require, "telescope")
 if not status then return end
 
-local fb_actions = require "telescope".extensions.file_browser.actions
-
-telescope.setup {
-  extensions = {
-    file_browser = {
-      theme = "ivy",
-      hijack_netrw = true,
-    },
-  },
-}
+telescope.setup {}
