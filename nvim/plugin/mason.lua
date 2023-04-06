5f935bf5ae9c2f35fec83c0dd3ba40c993f722a4
Arnaiz23 <aaf110501@gmail.com>
2023-04-06 20:01:51 +0200

    feat: 🎸 add mason config file

---
+++ nvim/plugin/mason.lua
@@ -0,0 +1,12 @@
+local status_mason, mason = pcall(require, "mason")
+if not status_mason then return end
+
+local status_mason_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_mason_lspconfig then return end
+
+mason.setup {}
+
+mason_lspconfig.setup {
+  ensure_installed = { "quick_lint_js", "tailwindcss", "cssls", "jsonls", "eslint" },
+  automatic_installation = true
+}
