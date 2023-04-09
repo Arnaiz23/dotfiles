eaea0823f8fcb41093faa701390a760794ce52e1
Arnaiz23 <aaf110501@gmail.com>
2023-04-09 22:08:36 +0200

    feat: 🎸 Add description for the key maps

--- nvim/lua/arnaiz/keybindings.lua
+++ nvim/lua/arnaiz/keybindings.lua
@@ -2,45 +2,43 @@ vim.g.mapleader = ","
 
 -- define a variable to create maps
 
-local opts = { noremap = true, silent = true }
-
-function Map(mode, shorcut, command)
-	vim.api.nvim_set_keymap(mode, shorcut, command, opts)
+function Map(mode, shorcut, command, desc)
+	vim.api.nvim_set_keymap(mode, shorcut, command, { noremap = true, silent = true, desc = desc })
 end
 
-function Nmap(shorcut, command)
-	Map('n', shorcut, command)
+function Nmap(shorcut, command, desc)
+	Map('n', shorcut, command, desc)
 end
 
 -- move beetween panels
-Nmap('<C-h>', '<C-w>h')
-Nmap('<C-l>', '<C-w>l')
-Nmap('<C-j>', '<C-w>j')
-Nmap('<C-k>', '<C-w>k')
+Nmap('<C-h>', '<C-w>h', 'Change left panel')
+Nmap('<C-l>', '<C-w>l', 'Change right panel')
+Nmap('<C-j>', '<C-w>j', 'Change down panel')
+Nmap('<C-k>', '<C-w>k', 'Change top panel')
 
 -- Work with the nvim tree
-Nmap('<leader>e', ':NvimTreeToggle<CR>')
-Nmap('<leader>o', ':NvimTreeFocus<CR>')
+Nmap('<leader>e', ':NvimTreeToggle<CR>', 'Toggle nvim tree')
+Nmap('<leader>o', ':NvimTreeFocus<CR>', 'Focus nvim tree')
 
 -- GIT
-Nmap('<leader>gb', ':Telescope git_branches<CR>')
-Nmap('<leader>gc', ':Telescope git_commits<CR>')
+Nmap('<leader>gb', ':Telescope git_branches<CR>', 'Show git branches')
+Nmap('<leader>gc', ':Telescope git_commits<CR>', 'Show git commits')
 --map('<leader>gl', ':Lazygit<CR>', opts)
 
 -- Save
-Nmap('<C-s>', ':w<CR>')
+Nmap('<C-s>', ':w<CR>', 'Save')
 
 -- Quit
-Nmap('<leader>q', ':q<CR>')
+Nmap('<leader>q', ':q<CR>', 'Quit')
 
 -- Find Files
-Nmap('<leader>f', ':Telescope find_files hidden=true<CR>')
+Nmap('<leader>f', ':Telescope find_files hidden=true<CR>', 'Search files')
 
 -- Change words
-Nmap('<leader>g', ':% s/')
+Nmap('<leader>c', ':% s/', 'Change words')
 
 -- Open new tab
-Nmap('<leader>t', ':tabnew<CR>')
+Nmap('<leader>t', ':tabnew<CR>', 'Open new tab')
 
 -- Show all the maps
-Nmap('<leader>m', ':Maps<CR>')
+-- Nmap('<leader>m', ':Maps<CR>')
