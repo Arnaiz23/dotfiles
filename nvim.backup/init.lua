e1236fbb373550ae6d832fcd9afc9bd31ac6bf6d
Arnaiz23 <aaf110501@gmail.com>
2023-04-09 16:16:12 +0200

    feat: 🎸 Modify the nvim folder and restore the nvim.backup

--- nvim.backup/init.lua
+++ nvim.backup/init.lua
@@ -3,13 +3,6 @@ vim.opt.termguicolors = true
 
 -- Set the colorscheme
 -- vim.cmd.colorscheme('onedark')
-local colorscheme = "onedark"
-local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-vim.o.background = "dark" -- or "light" for light mode
-if not ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
-end
 
 -- Plugins
 require("arnaiz.plugins")
