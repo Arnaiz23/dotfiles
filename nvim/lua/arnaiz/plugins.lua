a27857c6f19b70c6abf017a929983e576d089a20
Arnaiz23 <aaf110501@gmail.com>
2023-04-09 13:54:17 +0200

    feat: 🎸 Add a missing plugin

--- nvim/lua/arnaiz/plugins.lua
+++ nvim/lua/arnaiz/plugins.lua
@@ -29,6 +29,7 @@ packer.startup(function(use)
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+  use 'onsails/lspkind.nvim'
   use 'jose-elias-alvarez/null-ls.nvim'     -- Inject actions with LSP
   use 'MunifTanjim/prettier.nvim'           -- Prettier with LSP
   use 'glepnir/lspsaga.nvim'                -- LSP UI
