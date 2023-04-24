8f43baf6076ef1303011862c4d1b8dfdcc8d2d14
Arnaiz23 <aaf110501@gmail.com>
2023-04-24 11:08:25 +0200

    fix: Change the lspsaga jump to another map

--- nvim/plugin/lspsaga.lua
+++ nvim/plugin/lspsaga.lua
@@ -8,7 +8,7 @@ saga.setup {
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-e>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-p>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
