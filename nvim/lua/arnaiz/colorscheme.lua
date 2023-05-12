146e94841301f85ce272c42ba358d228f36a171f
Arnaiz23 <aaf110501@gmail.com>
2023-05-12 13:06:22 +0200

    feat: 🎸 Delete the neovim configuration

--- nvim/lua/arnaiz/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local colorscheme = "tokyonight-moon"
---local colorscheme = "onedark"
-local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-vim.o.background = "dark" -- or "light" for light mode
-if not ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
-end
