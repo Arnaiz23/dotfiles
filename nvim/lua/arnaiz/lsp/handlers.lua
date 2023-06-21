883eefebed285725178e817e473fb3396cf04e04
Arnaiz23 <aaf110501@gmail.com>
2023-06-21 18:52:59 +0200

    feat: 🎸 Change the lsp keybindings and add the desc field

--- nvim/lua/arnaiz/lsp/handlers.lua
+++ nvim/lua/arnaiz/lsp/handlers.lua
@@ -52,23 +52,33 @@ M.setup = function()
 end
 
 local function lsp_keymaps(bufnr)
+	function options(desc)
+		local opts = { noremap = true, silent = true, desc = desc }
+		return opts
+	end
+
 	local opts = { noremap = true, silent = true }
+
 	local keymap = vim.api.nvim_buf_set_keymap
-	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
-	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-	keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
-	keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
-	keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
-	keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	-- keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", options("Format code"))
+	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", options("LSP info"))
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lj",
+		"<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>",
+		options("Go to the next error")
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lk",
+		"<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
+		options("Go to the prev error")
+	)
+	keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", options("Rename"))
 end
 
 M.on_attach = function(client, bufnr)
