a92c361731b06f54a398854abd8c9538d4036674
Arnaiz23 <aaf110501@gmail.com>
2023-06-25 21:53:19 +0200

    feat: 🎸 Add the environment script

---
+++ installers/environment.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+options=("qtile" "gnome")
+
+PS3="Select an option: "
+
+select i in ${options[@]}
+do
+    case $i in
+        "qtile")
+            sudo pacman -S qtile pacman-contrib --noconfirm
+            ln -s ~/.dotfiles/qtile ~/.config/qtile
+            clear
+            echo "Restart the pc and select qtile before signin."
+            exit
+            ;;
+        "gnome")
+            sudo pacman -S --noconfirm xorg xorg-server gnome
+            clear
+            sudo systemctl start gdm.service
+            sudo systemctl enable gdm.service
+            clear
+            sudo pacman -S --noconfirm gnome-tweaks
+            echo "Restart the pc and select gnome before signin."
+            exit
+            ;;
+        *) echo "Invalid option" ;;
+    esac
+done
