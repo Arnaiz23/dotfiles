60ae8e337acc1fc4fc8eab911e963f3c6ea18dd5
Arnaiz23 <aaf110501@gmail.com>
2023-06-27 12:47:51 +0200

    feat: 🎸 Errors hovers

--- nvim/lua/arnaiz/lsp/init.lua
+++ nvim/lua/arnaiz/lsp/init.lua
@@ -6,3 +6,7 @@ end
 require("arnaiz.lsp.mason")
 require("arnaiz.lsp.handlers").setup()
 require("arnaiz.lsp.null-ls")
+
+-- Errors hover
+vim.o.updatetime = 250
+vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
