8812bed14be06d93912d7e1b8a87b0f541b2ec7f
Arnaiz23 <aaf110501@gmail.com>
2023-09-12 00:52:11 +0200

    fix 🐛: Create the nvim-tree setup in init.lua
     Create the setup in the root file because an error with the PackerUpdate

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -8,7 +8,7 @@ require("arnaiz.cmp")
 require("arnaiz.lsp")
 require("arnaiz.indent")
 require("arnaiz.treesitter")
-require("arnaiz.nvim-tree")
+-- require("arnaiz.nvim-tree")
 require("arnaiz.lualine")
 require("arnaiz.bufferline")
 require("arnaiz.comment")
@@ -19,3 +19,15 @@ require("arnaiz.notify")
 require("arnaiz.lspsaga")
 require("arnaiz.which-key")
 require("arnaiz.web-devicons")
+
+
+-- Nvim-tree setup (I don't know why nvim don't recognize the nvim-tree file setup)
+require("nvim-tree").setup {
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true
+  },
+  filters = {
+    dotfiles = false
+  }
+}
