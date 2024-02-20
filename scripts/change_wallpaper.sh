5e40fe2e7e556034cd8fd75ca600ff0351a8e359
Arnaiz23 <aaf110501@gmail.com>
2024-02-20 21:14:35 +0100

    feat ✨: Update the bg script with feh, fzf preview catimg

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -1,10 +1,12 @@
-image_name="ai4.png"
 images_url=$HOME/Imágenes/wallpaper-4k
+image_name=$(
+    ls $images_url | fzf --preview="catimg -w 200 $images_url/{}"
+)
 
 full_url="$images_url/$image_name"
 
 if [ -f $full_url ]; then
-  feh --bg-scale $full_url
+    feh --bg-scale $full_url
 else
     echo "This image doesn't exists"
 fi
