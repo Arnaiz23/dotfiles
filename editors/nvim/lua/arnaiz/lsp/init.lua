2f37c62ab23f3d2e8d26c5b561c3445dd4ef1bfa
Arnaiz23 <aaf110501@gmail.com>
2024-06-02 17:38:40 +0200

    feat ✨: Add AutoFormat for LSP

--- editors/nvim/lua/arnaiz/lsp/init.lua
+++ editors/nvim/lua/arnaiz/lsp/init.lua
@@ -1,6 +1,6 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-	return
+  return
 end
 
 require("arnaiz.lsp.mason")
@@ -10,3 +10,17 @@ require("arnaiz.lsp.null-ls")
 -- Errors hover
 vim.o.updatetime = 250
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+
+-- Autoformat
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        -- vim.lsp.buf.format { async = false, id = args.data.client_id }
+        vim.lsp.buf.format { async = true }
+      end,
+    })
+  end
+})
