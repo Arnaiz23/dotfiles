9f540bab423aa468385fcbc1a45b96934fc98ac5
Arnaiz23 <aaf110501@gmail.com>
2025-02-12 21:07:48 +0100

    feat ✨: add polybar configuration

---
+++ os/linux/polybar/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+if type "xrandr" > /dev/null; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload mainbar-qtile -c ~/.config/polybar/config.ini &
+	done
+else
+	polybar --reload mainbar-qtile -c ~/.config/polybar/config.ini &
+fi
