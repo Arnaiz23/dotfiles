a7211431b3a027d8c227f37b0cf62da85f86801b
Arnaiz23 <aaf110501@gmail.com>
2025-02-25 20:06:02 +0100

    feat ✨: update the qtile autostart file

--- os/linux/qtile/autostart.sh
+++ os/linux/qtile/autostart.sh
@@ -18,7 +18,10 @@ picom &
 blueman-applet &
 
 # Polybar
-~/.config/polybar/launch.sh $
+~/.config/polybar/launch.sh &
 
 dbus-update-activation-environment --all
 gnome-keyring-daemon --start --components=secrets
+
+xset s -dpms
+xset s off
