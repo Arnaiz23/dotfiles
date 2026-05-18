620475da851de55ef50ee5f228ce80300a962a8c
Arnaiz23 <aaf110501@gmail.com>
2026-05-18 16:49:13 +0200

    fix : migrate lspconfig setup to the new nvim (+0.11)

--- editors/nvim/lua/arnaiz/lsp/mason.lua
+++ editors/nvim/lua/arnaiz/lsp/mason.lua
@@ -40,7 +40,7 @@ require("mason-lspconfig").setup({
 	automatic_installation = true,
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+local lspconfig_status_ok, _ = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
 	return
 end
@@ -60,5 +60,5 @@ for _, server in pairs(servers) do
 		opts = vim.tbl_deep_extend("force", conf_opts, opts)
 	end
 
-	lspconfig[server].setup(opts)
+	vim.lsp.config(server, opts)
 end
